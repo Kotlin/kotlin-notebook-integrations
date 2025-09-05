37d77aca6d2618cabeae0ce0d30278bceb7ba1d0
Ilya Muradyan <ilya.muradyan@jetbrains.com>
2025-09-05 15:06:55 +0200

    Do basic setup of dependencies for Jupyter integration

--- integrations/database/build.gradle.kts
+++ integrations/database/build.gradle.kts
@@ -1,22 +1,94 @@
+import org.jetbrains.kotlinx.publisher.apache2
+import org.jetbrains.kotlinx.publisher.developer
+
 plugins {
-    kotlin("jvm") version "2.2.10"
+    alias(libs.plugins.kotlin.jvm)
+    alias(libs.plugins.kotlin.jupyter.api)
+    alias(libs.plugins.publisher)
+    alias(libs.plugins.ktlint)
 }
 
+val spaceUsername: String by properties
+val spaceToken: String by properties
+
 group = "org.jetbrains.kotlinx"
-version = "1.0-SNAPSHOT"
+version =
+    detectVersion().also { version ->
+        Logging.getLogger(this::class.java).warn("Detected version: $version")
+    }
+
+allprojects {
+    version = rootProject.version
+}
+
+private fun detectVersion(): String {
+    val buildNumber = project.findProperty("build.number")
+    if (buildNumber != null) {
+        return buildNumber.toString()
+    } else {
+        val baseVersion = project.property("baseVersion").toString()
+        val devAddition = project.property("devAddition").toString()
+        return "$baseVersion-$devAddition-SNAPSHOT"
+    }
+}
+
+kotlinJupyter {
+    addApiDependency()
+}
+
+tasks.processJupyterApiResources {
+    libraryProducers = listOf("org.jetbrains.kotlinx.jupyter.database.DatabaseJupyterIntegration")
+}
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
+    // api(libs.kotlin.jupyter.lib)
+    // api(libs.dataframe.core)
     testImplementation(kotlin("test"))
-}
-
-kotlin {
-    jvmToolchain(17)
 }
 
 tasks.test {
     useJUnitPlatform()
+}
+
+kotlin {
+    jvmToolchain(libs.versions.jvmTarget.get().toInt())
+}
+
+kotlinPublications {
+    defaultGroup.set(group.toString())
+    fairDokkaJars.set(false)
+
+    pom {
+        inceptionYear.set("2025")
+        licenses {
+            apache2()
+        }
+        developers {
+            developer("ileasile", "Ilya Muradyan", "Ilya.Muradyan@jetbrains.com")
+        }
+    }
+
+    localRepositories {
+        localMavenRepository(project.layout.buildDirectory.dir("maven"))
+    }
+
+    remoteRepositories {
+        maven {
+            name = "kotlin-ds-maven"
+            url = uri("https://packages.jetbrains.team/maven/p/kds/kotlin-ds-maven")
+            credentials {
+                username = spaceUsername
+                password = spaceToken
+            }
+        }
+    }
+
+    publication {
+        publicationName.set("kotlin-jupyter-database")
+        description.set("Kotlin Jupyter kernel integration with DBMSs")
+    }
 }
