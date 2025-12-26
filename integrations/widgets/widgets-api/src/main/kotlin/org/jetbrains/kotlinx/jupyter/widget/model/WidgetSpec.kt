5592116d313635637fa8056b7c9e3c055c331922
Ilya Muradyan <ilya.muradyan@jetbrains.com>
2025-12-26 06:20:45 +0100

    KTNB-1205: Remove unused widget specs and related functions, clean up version constraint handling.

--- integrations/widgets/widgets-api/src/main/kotlin/org/jetbrains/kotlinx/jupyter/widget/model/WidgetSpec.kt
+++ integrations/widgets/widgets-api/src/main/kotlin/org/jetbrains/kotlinx/jupyter/widget/model/WidgetSpec.kt
@@ -4,9 +4,7 @@ import kotlinx.serialization.Serializable
 
 internal const val DEFAULT_MAJOR_VERSION = 2
 internal const val DEFAULT_MINOR_VERSION = 0
-internal const val DEFAULT_PATCH_VERSION = 0
 
-private const val DEFAULT_VERSION_CONSTRAINT = "^$DEFAULT_MAJOR_VERSION.$DEFAULT_MINOR_VERSION.$DEFAULT_PATCH_VERSION"
 internal val versionConstraintRegex = Regex("""\D*(\d+)\.(\d+)""")
 
 /**
@@ -22,32 +20,3 @@ public data class WidgetSpec(
     val viewModule: String,
     val viewModuleVersion: String,
 )
-
-public fun controlsSpec(
-    controlName: String,
-    versionConstraint: String = DEFAULT_VERSION_CONSTRAINT,
-): WidgetSpec = iPyWidgetsSpec(controlName, "@jupyter-widgets/controls", versionConstraint)
-
-public fun baseSpec(
-    controlName: String,
-    versionConstraint: String = DEFAULT_VERSION_CONSTRAINT,
-): WidgetSpec = iPyWidgetsSpec(controlName, "@jupyter-widgets/base", versionConstraint)
-
-public fun outputSpec(
-    controlName: String,
-    versionConstraint: String = DEFAULT_VERSION_CONSTRAINT,
-): WidgetSpec = iPyWidgetsSpec(controlName, "@jupyter-widgets/output", versionConstraint)
-
-public fun iPyWidgetsSpec(
-    controlName: String,
-    moduleName: String,
-    versionConstraint: String = DEFAULT_VERSION_CONSTRAINT,
-): WidgetSpec =
-    WidgetSpec(
-        modelName = controlName + "Model",
-        modelModule = moduleName,
-        modelModuleVersion = versionConstraint,
-        viewName = controlName + "View",
-        viewModule = moduleName,
-        viewModuleVersion = versionConstraint,
-    )
